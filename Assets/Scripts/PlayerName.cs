25f408ef94d1088718dd41dab6209388ef529190
Ik4rCat <119232131+Ik4rCat@users.noreply.github.com>
2024-06-12 18:00:39 +0300

    player-Name-display

---
+++ Assets/Scripts/PlayerName.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using TMPro;
+
+public class PlayerName : NetworkBehaviour
+{
+    [SerializeField] TMP_Text playerNameText;
+    [SyncVar(hook = nameof(OnPlayerNameUpdated))] string playerName;
+
+    public override void OnStartServer()
+    {
+        playerName = $"Player: {connectionToClient.connectionId}";
+    }
+    
+    private void OnPlayerNameUpdated(string oldValue, string newValue)
+    {
+        playerNameText.text = newValue; 
+
+    }
+
+}
