36f87c0c38b0bcc76c825d1d9299cfb23859488f
Ik4rCat <119232131+Ik4rCat@users.noreply.github.com>
2024-08-14 17:29:30 +0300

    RestartButtonUpgrade

--- Assets/Scripts/GameOverDisplay.cs
+++ Assets/Scripts/GameOverDisplay.cs
@@ -1,27 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 using TMPro;
+using UnityEngine.UI;
 
 public class GameOverDisplay : MonoBehaviour
 {
     [SerializeField] Canvas canvas;
     [SerializeField] TMP_Text winerNameText;
+    [SerializeField] Button restartButton;
 
     private void OnEnable()
     {
         GameOverHandler.ClientGameOver += ClientOnGameOver;
+        restartButton.onClick.AddListener(RestartGame); 
     }
 
     private void OnDisable()
     {
         GameOverHandler.ClientGameOver -= ClientOnGameOver; 
+        restartButton.onClick.RemoveListener(RestartGame);
     }
 
     private void ClientOnGameOver(string winner)
     {
         canvas.enabled = true;
         winerNameText.text = $"{winner} won! ";
+    }
+
+    private void RestartGame()
+    {
+        if (NetworkServer.active && NetworkClient.isConnected)
+            NetworkManager.singleton.StopHost();
+        else
+            NetworkManager.singleton.StopClient();
+        
+        canvas.enabled = false;
     }
 
 }
